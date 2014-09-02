25cc86ea27e38cacaa44a1acf7e30df12eba0372
jsloman <JonathanS2@gmail.com>
2014-09-02 21:23:00 +0100

    initial commit

---
+++ android/src/uk/me/jonathans/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package uk.me.jonathans.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import uk.me.jonathans.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/uk/me/jonathans/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.me.jonathans.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.me.jonathans.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
